a464ddadfaa7697b1322d0e5e013d0f2eae3e17a
redeagle0705 <69983751+redeagle0705@users.noreply.github.com>
2024-05-03 18:24:02 +0900

    11
    11

--- scripts/reload.js
+++
@@ -1,8 +0,0 @@
-
-var atlas = require("atlas");
-
-module.exports = () => 
-{
-	atlas.setupIcon("block","silicon-smelter",Blocks.siliconSmelter.region);
-	atlas.setupIcon("block","kiln",Blocks.kiln.region);
-}
