421061d3961b7bb926847a690b67c55619b1f76f
redeagle0705 <69983751+redeagle0705@users.noreply.github.com>
2025-02-08 01:18:26 +0900

    cvn
    vcn

---
+++ scripts/mian.js
@@ -0,0 +1,4 @@
+Blocks.siliconSmelter.drawer=new DrawMulti(new DrawDefault(), new DrawFade());
+Blocks.siliconCrucible.drawer=new DrawMulti(new DrawDefault(), new DrawFade());
+Blocks.kiln.drawer=new DrawMulti(new DrawDefault(), new DrawFade());
+Blocks.surgeSmelter.drawer=new DrawMulti(new DrawDefault(), new DrawFade());
